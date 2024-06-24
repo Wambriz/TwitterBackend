6cc33a4a78b798b69b4c2ef3425a66e52d47b35c
Jacob Taylor <jtlikesps4@icloud.com>
2024-06-24 11:02:20 -0500

    created project with mapstruct,application properties and gitignore modified

---
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/GroupProject1Team1Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.group_project_1_team_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProject1Team1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProject1Team1Application.class, args);
+	}
+
+}
---
+++ group-project-1-team-1/group-project-1-team-1/src/test/java/com/cooksys/group_project_1_team_1/GroupProject1Team1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.group_project_1_team_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupProject1Team1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
