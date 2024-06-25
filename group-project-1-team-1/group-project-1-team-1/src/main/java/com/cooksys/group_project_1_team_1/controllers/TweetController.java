dcc7296060b5346ffbc1275af8072a7a8d0f78a9
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-06-25 14:24:00 -0500

    Update TweetController.java

--- group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/controllers/TweetController.java
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/controllers/TweetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("tweets")
+@RequestMapping("/tweets")
 public class TweetController {
 
 }
