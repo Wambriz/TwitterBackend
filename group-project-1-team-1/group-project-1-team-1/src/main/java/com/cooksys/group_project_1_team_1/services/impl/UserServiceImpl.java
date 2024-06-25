2ebde32c376459b0a0c1b38c42a15649573bee1f
Jacob Taylor <jtlikesps4@icloud.com>
2024-06-24 19:37:45 -0500

    Added hashtagService,UserService and ValidateService

---
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooksys.group_project_1_team_1.services.impl;
+
+import com.cooksys.group_project_1_team_1.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+}
---
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooksys.group_project_1_team_1.services.impl;
+
+import com.cooksys.group_project_1_team_1.services.ValidateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl implements ValidateService {
+}
