2279dc62ed38739f52e45616e4621eaa923b57c7
Jacob Taylor <jtlikesps4@icloud.com>
2024-06-25 16:02:44 -0500

    Changed dtos to only use dtos

--- group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/controllers/ValidateController.java
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/controllers/ValidateController.java
@@ -2,6 +2,7 @@ package com.cooksys.group_project_1_team_1.controllers;
 
 import com.cooksys.group_project_1_team_1.services.ValidateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/repositories/TweetRepository.java
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/repositories/TweetRepository.java
@@ -2,6 +2,7 @@ package com.cooksys.group_project_1_team_1.repositories;
 
 import com.cooksys.group_project_1_team_1.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
