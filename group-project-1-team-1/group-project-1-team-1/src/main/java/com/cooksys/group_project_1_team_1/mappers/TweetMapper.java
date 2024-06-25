2d2ccffb67c4d1c2a405a5fc3e0661d04da2a41c
Jacob Taylor <jtlikesps4@icloud.com>
2024-06-25 13:51:06 -0500

    Added mapper annotation to tweetMapper

--- group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/mappers/TweetMapper.java
+++ group-project-1-team-1/group-project-1-team-1/src/main/java/com/cooksys/group_project_1_team_1/mappers/TweetMapper.java
@@ -3,7 +3,9 @@ package com.cooksys.group_project_1_team_1.mappers;
 import com.cooksys.group_project_1_team_1.entities.Tweet;
 import com.cooksys.group_project_1_team_1.models.TweetRequestDto;
 import com.cooksys.group_project_1_team_1.models.TweetResponseDto;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring",uses = {UserMapper.class})
 public interface TweetMapper {
     Tweet requestDtoToEntity(TweetRequestDto tweetRequestDto);
     Tweet responseDtoToEntity(TweetResponseDto tweetResponseDto);
